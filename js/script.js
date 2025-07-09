0fe29a28dd2be8c0ab11d8440d805eabc8e52b7d
Mitsuki <ne221164@senshu-u.jp>
2025-07-09 09:12:04 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function showPageByHash() {
 	if (target) {
 		target.classList.add('active');
 	} else {
-		document.getElementById('home').classList.add('active');
+		document.getElementById('works').classList.add('active');
 	}
 }
 
@@ -18,11 +18,11 @@ window.addEventListener('DOMContentLoaded', showPageByHash);
 window.addEventListener('hashchange', showPageByHash);
 
 // Adobe Fonts
-(function (d) {
-	var config = {
-		kitId: 'iyg6apw',
-		scriptTimeout: 3000,
-		async: true
-	},
-		h = d.documentElement, t = setTimeout(function () { h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive"; }, config.scriptTimeout), tk = d.createElement("script"), f = false, s = d.getElementsByTagName("script")[0], a; h.className += " wf-loading"; tk.src = 'https://use.typekit.net/' + config.kitId + '.js'; tk.async = true; tk.onload = tk.onreadystatechange = function () { a = this.readyState; if (f || a && a != "complete" && a != "loaded") return; f = true; clearTimeout(t); try { Typekit.load(config) } catch (e) { } }; s.parentNode.insertBefore(tk, s)
-})(document);
+(function(d) {
+    var config = {
+      kitId: 'qii1awd',
+      scriptTimeout: 3000,
+      async: true
+    },
+    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+  })(document);
